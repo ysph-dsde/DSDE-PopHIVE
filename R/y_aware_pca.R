832c90f802a702b12e586aa007536d211ac2535b
Weinberger <daniel.weinberger@yale.edu>
2025-03-27 22:24:47 -0400

    y aware pca

--- R/y_aware_pca.R
+++ R/y_aware_pca.R
@@ -20,6 +20,7 @@ library(ggrepel)
 library(readxl)
 library(ggalluvial)
 library(waffle)
+library(INLA)
 
 a1 <- read.csv('./Data/plot_files/dwh_combined_plot1.csv') %>%
   dplyr::select(state, date, 
@@ -43,14 +44,14 @@ a1 <- read.csv('./Data/plot_files/dwh_combined_plot1.csv') %>%
           lag6 = lag(value,6),
           lag7 = lag(value,7),
           lag8 = lag(value,8),
-          lag9 = lag(value,9),
-          lag10 = lag(value,10),
-          lag11 = lag(value,11),
-          lag12 = lag(value,12),
-          lag13 = lag(value,13),
-          lag14 = lag(value,14),
-          lag15 = lag(value,15),
-          lag16 = lag(value,16),
+        #  lag9 = lag(value,9),
+         # lag10 = lag(value,10),
+        #  lag11 = lag(value,11),
+        #  lag12 = lag(value,12),
+        #  lag13 = lag(value,13),
+        #  lag14 = lag(value,14),
+        #  lag15 = lag(value,15),
+        #  lag16 = lag(value,16),
   ) %>%
   rename(variable1=variable) %>%
   ungroup() %>%
@@ -72,6 +73,7 @@ a2 <- read.csv('./Data/plot_files/e1_age_epic_age_rsv.csv') %>%
   group_by(state,agec) %>%
   rename(value=N_cases_epic) %>%
   mutate( 
+    value = if_else(is.na(value), min(value, na.rm=T)/2, value), #if missing/suppressed, set to half of minimum recorded value
     lag0 = lag(value,0),
     lag4 = lag(value,4),
     lag5 = lag(value,5),
@@ -99,6 +101,7 @@ a2 <- read.csv('./Data/plot_files/e1_age_epic_age_rsv.csv') %>%
   filter(!is.na(scale_value)) %>%
   reshape2::dcast(date~variable+agec, value.var='scale_value') 
 
+plot(a2$`lag5_u1 Years_New York`)
 
 ##NSSP
 a3 <- open_dataset('./Data/NSSP_detailed.parquet') %>%
@@ -121,7 +124,8 @@ a3 <- open_dataset('./Data/NSSP_detailed.parquet') %>%
   mutate(geo = paste(state, county, sep='_')) %>%
   rename(date=week_end) %>%
   group_by(geo) %>%
-  mutate(scale_value=percent_visits_rsv/max(percent_visits_rsv, na.rm=T),
+  mutate(percent_visits_rsv= if_else(is.na(percent_visits_rsv), min(percent_visits_rsv,na.rm=T)/2, percent_visits_rsv), #fill in missing suppressed values
+         scale_value=percent_visits_rsv/max(percent_visits_rsv, na.rm=T),
          variable1='nssp') %>%
   dplyr::select(geo,date,variable1, scale_value) %>%
   filter(!is.na(scale_value)) %>%
@@ -233,14 +237,16 @@ g1_metro <- read_parquet(temp_file1) %>%
 ##Combine the variables into one large dataframe
 
 all.y <- a2 %>%
-  dplyr::select(date, `lag0_u1 Years_New York`) %>%
-  rename(Y=`lag0_u1 Years_New York`) %>%
+  #dplyr::select(date, `lag0_u1 Years_New York`) %>%
+  dplyr::select(date, `lag0_1_4 Years_New York`) %>%
+  rename(Y=`lag0_1_4 Years_New York`) %>%
   mutate(variable='EpicAgeRSV' ) 
 
 all.vars<- a2 %>%  #exclude a1
   full_join(a3, by='date') %>%
   full_join(g1_metro, by='date') %>%
-  right_join(all.y, by='date')
+  right_join(all.y, by='date') %>%
+  filter(date<='2025-1-14' & date>='2023-10-01')
 
 X <- all.vars %>%
   dplyr::select(-starts_with('lag0'), -date,-starts_with('N_cases_epic'),-Y ) %>%
@@ -265,31 +271,35 @@ library(pls)
 
 X_imputed <- softImpute::complete(X.scale, softImpute(X.scale))
 
-model <- plsr(Y.fit ~ X_imputed, ncomp = 20, validation = "CV")
+X_imputed_rescale <- apply(X_imputed,2, function(xx) (xx- mean(xx,na.rm=T))/sd(xx,na.rm=T) )
+
+#model <- plsr(Y.fit ~ X_imputed_rescale,  validation = "CV", segments=5, segment.type = c("consecutive") )
 
 # Check cross-validation results
-summary(model)
+#summary(model)
 
 # Plot RMSE to find the optimal number of components
-plot(RMSEP(model), legendpos = "topright")
+#plot(RMSEP(model), legendpos = "topright")
 
 # Get the best number of components
-best_ncomp <- which.min(RMSEP(model)$val['CV', 'Y.fit', ])
+#best_ncomp <- which.min(RMSEP(model)$val['CV', 'Y.fit', ])
 
+best_ncomp = 10
 cat("Optimal number of components:", best_ncomp)
 
 # Refit the final model with the optimal number of components
-final_model <- plsr(Y.fit ~ X_imputed, ncomp = best_ncomp)
+final_model <- pls::plsr(Y.fit ~ X_imputed, ncomp = best_ncomp)
 
 preds.pls <- all.vars %>%
   dplyr::select(date) %>%
   mutate(pred1=predict(final_model, ncomp = best_ncomp, newdata=as.data.frame(X_imputed)), Y=Y, Y.fit=Y.fit)
 
-preds.pls %>%
+p1 <-  preds.pls %>%
   ggplot()+
   geom_line(aes(x=date, y=pred1), col='red')+
   geom_line(aes(x=date, y=Y))+
   theme_minimal()
+ggplotly(p1)
 
 loadings <- as.data.frame(final_model$loadings[, 1:best_ncomp])
 
@@ -308,47 +318,75 @@ vip_df <- cbind.data.frame(Variable = colnames(X), VIP = vip_scores)
 vip_df <- vip_df[order(-vip_df$VIP), ]
 print(vip_df)
 
-####################################################
-####################################################
-# 
-# ## SUPERVISED PCA
-# 
-# Y.scale <- ( Y - mean(Y, na.rm=T) )/ sd(Y,na.rm=T)
-# 
-# y.aware.scale<- apply(X_imputed, 2, function(x1){
-#   x1 <- as.vector(x1)
-#     reg<-lm(Y.scale~x1)
-#   slope<- reg$coefficients[2]
-#   x.scale<- x1*slope - mean(x1*slope)
-#   return(x.scale)
-# })
-# 
-# y.aware.scale_nomiss <- y.aware.scale[,colSums(is.na(y.aware.scale)) == 0]
-# 
-# pca1<- prcomp(y.aware.scale_nomiss, center = FALSE,scale. = FALSE)
-# 
-# # plot(pca1$sdev)
-# n.pcs.keep<-10
-# pcs<-pca1$x
-# pcs<- as.data.frame(apply(pcs,2, scale)) #SCALE THE PCS prior to regression!
-# 
-# names(pcs) <- paste0('PC', 1:ncol(pcs))
-# 
-# pc.df <- cbind.data.frame('date'=all.vars$date, 'Y'=Y,  pcs[,1:n.pcs.keep])
-# 
-# mod1 <- lm(Y ~ PC1 +PC2, data=pc.df  )
-# summary(mod1)
-# pc.df$pred1 <- predict(mod1, newdata = pc.df)
-# 
-# 
-# loadings <- pca1$rotation[,1:10]
-# 
-# pc.df %>%
-#   ggplot()+
-#   geom_line(aes(x=date, y=pred1))+
-#   geom_line(aes(x=date, y=Y))+
-#   theme_minimal()
 
+#####################################
+
+##Y-AWARE PCA
+
+y.aware.scale<- apply(X_imputed, 2, function(x1){
+  x1 <- as.vector(x1)
+
+  reg<-lm(Y.fit~x1)
+  slope<- reg$coefficients[2]
+  x.scale<-x1*slope - mean(x1*slope)
+  return(x.scale)
+})
+
+y.aware.scale <- y.aware.scale[ , colSums(is.na(y.aware.scale)) == 0]
+
+pca1<- prcomp(y.aware.scale, center = FALSE,scale. = FALSE)
+# plot(pca1$sdev)
+n.pcs.keep<-5
+pcs<-pca1$x
+pcs<- as.data.frame(apply(pcs,2, scale)) #SCALE THE PCS prior to regression!
+names(pcs) <- paste0('PC', 1:ncol(pcs))
+pc.df <- cbind.data.frame('date'=all.vars$date,'Y.fit'=Y.fit,'t'=1:length(Y), 'Y'=Y, pcs[,1:n.pcs.keep])
+plot(pcs[,4])
+
+
+mod1 <- lm(Y.fit~ PC1 + PC2 +PC3, data=pc.df )
+summary(mod1)
+pc.df$pred1 <- predict(mod1, newdata=pc.df)
+
+pc.df%>%
+ggplot()+
+  geom_line(aes(x=date, y=pred1), col='red')+
+  geom_line(aes(x=date, y=Y), col='black')
+
+#############################################
+##INLA mod with RW
+eqn1 <- as.formula('Y.fit ~ PC1 +PC2+PC3 +PC4+ f(t, model="rw1") '  )
+
+mod1 <- inla(eqn1, data = pc.df,  family = "gaussian",
+             control.compute = list(dic = FALSE, 
+                                    waic = FALSE, 
+                                    config = T,
+                                    return.marginals=F
+             ),
+             # save predicted values on response scale
+             control.predictor = list(compute=TRUE, link=1),
+             control.inla = list(strategy='adaptive', # adaptive gaussian
+                                 cmin=0),
+             control.fixed = list(mean.intercept=0, 
+                                  prec.intercept=0.04, # precision 1
+                                  mean=0, 
+                                  prec=1), # weakly regularising on fixed effects (sd of 1)
+             inla.mode = "experimental", # new version of INLA algorithm (requires R 4.1 and INLA testing version)
+             num.threads=8
+)    
+
+
+pc.df$inla_pred <- mod1$summary.fitted.values$mean
+
+pc.df%>%
+  ggplot()+
+  geom_line(aes(x=date, y=inla_pred), col='red')+
+  geom_line(aes(x=date, y=Y), col='black')
+
+###############################################
+
+
+###################################
 
 comp.Y <- a2 %>%
   rename(NY=`lag0_u1 Years_New York`, 
@@ -401,3 +439,30 @@ test1  <- read.csv('./Data/plot_files/e1_age_epic_age_rsv.csv') %>%
   reshape2::dcast(state~epiyr, value.var='rsv_int') %>%
   rename(yr2024=`2024`, yr2023=`2023`) %>%
   mutate(ratio=yr2024/yr2023)
+
+
+## Kalman filter (code from gpt-4o)
+library(MARSS)
+
+length.y <- nrow(all.vars)
+all.vars.fit <- all.vars 
+all.vars.fit[(length.y-2):length.y,'Y'] <-  NA
+all.vars.fit$date <- NULL
+all.vars.fit.mat <- matrix(as.numeric(as.matrix(all.vars.fit)), ncol=ncol(all.vars.fit))
+
+# Optionally, set initial state and observation variance-covariance matrices if needed
+model.list <- list()
+
+fit <- MARSS(all.vars.fit.mat, model = model.list)
+
+# Check if the fit converged
+if (!is.na(fit$convergence)) {
+  print("Convergence achieved.")
+} else {
+  print("Convergence not achieved.")
+}
+
+# Impute the missing values in the state-space model
+imputed_data <- fit$ytT  # State estimates including missing values
+imputed_data <- t(imputed_data)  # Transpose back to original form
+
