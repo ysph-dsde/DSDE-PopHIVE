d4596e386c874d43765fcece6cdcfb12e2c920f0
Weinberger <daniel.weinberger@yale.edu>
2025-03-30 16:41:46 -0400

    adds links and refresh some data

--- R/DataPull.R
+++ R/DataPull.R
@@ -104,16 +104,6 @@ verify_update(  test_file = cdc_rsvnet, ds_path='./Data/rsvnet/') %>%
 h1.age <- cdc_rsvnet %>%
   filter(state!="RSV-NET" & sex=='All' & race=='All' & type=='Crude Rate') %>%
   rename( hosp_rate=rate, date=week_ending_date, Level=age_category) %>%
-  filter(Level %in% c('1-4 years', '0-<1 year','5-17 years', '18-49 years' ,
-                      "≥65 years" ,"50-64 years" )) %>%
-  mutate( Level = if_else(Level=='0-<1 year',"<1 Years",
-                          if_else( Level=='1-4 years', "1-4 Years",
-                          if_else(Level=="5-17 years" ,"5-17 Years",
-                          if_else(Level=="18-49 years" ,"18-49 Years",
-                          if_else(Level=="50-64 years" ,"50-64 Years",
-                           if_else(Level=="≥65 years",'65+ Years', 'other'               
-                                  ))))))
-  ) %>%
   dplyr::select(state, date, hosp_rate, Level) %>%
   ungroup() %>%
   filter( date >=as.Date('2023-07-01')) %>%
